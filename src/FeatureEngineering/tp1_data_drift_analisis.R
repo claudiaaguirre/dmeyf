c8298588ed55cc026e28e80e96acf4be43e17b0d
Claudia Aguirre <claudiaaguirre83@gmail.com>
2021-09-30 12:32:54 -0300

    Data drift: se prueba base model, sacando una segunda tanta de parametros e igualmente el modelo logra distinguir perfectamente un registro de un mes, respecto del otro

--- src/FeatureEngineering/tp1_data_drift_analisis.R
+++ src/FeatureEngineering/tp1_data_drift_analisis.R
@@ -10,35 +10,37 @@ setwd("/Users/claudia/DMenEyF/") #establezco la carpeta donde voy a trabajar
 datasetA  <- fread( "./datasetsOri/paquete_premium_202009.csv" )
 datasetB  <- fread( "./datasetsOri/paquete_premium_202011.csv" )
 
-campos_buenos <-  setdiff(  colnames( datasetA),  
-                            c("numero_de_cliente","foto_mes","clase_ternaria") )
-
-#-------------------------------------------------------------
-#--------Kolmogorov–Smirnov test (K–S test or KS test)--------
-# Do x and y come from the same distribution?
-#-------------------------------------------------------------
+campos_buenos  <- setdiff(  colnames(datasetA),  c("clase_ternaria", "numero_de_cliente","foto_mes", 
+                                                        "internet",
+                                                   "tpaquete1",
+                                                   "mcuenta_corriente",
+                                                   "mcaja_ahorro_dolares",
+                                                   "mcuentas_saldo",
+                                                   "minversion1_pesos",
+                                                   "mpayroll",
+                                                   "mpagodeservicios",
+                                                   "mcajeros_propios_descuentos",
+                                                   "ccajeros_propios_descuentos",
+                                                   "ctarjeta_visa_descuentos",
+                                                   "mtarjeta_visa_descuentos",
+                                                   "ctarjeta_master_descuentos",
+                                                   "mtarjeta_master_descuentos",
+                                                   "mforex_buy",
+                                                   "matm_other",
+                                                   "tmobile_app",
+                                                   "cmobile_app_trx",
+                                                   "Master_Finiciomora",
+                                                   "Master_mconsumosdolares",
+                                                   "Master_madelantopesos",
+                                                   "Master_madelantodolares",
+                                                   "Visa_mfinanciacion_limite",
+                                                   "Visa_Finiciomora",
+                                                   "Visa_msaldodolares",
+                                                   "Visa_mpagado",
+                                                   "Visa_mpagominimo"
+                                                   ) )
 dA = datasetA[, mget(campos_buenos)]
 dB = datasetB[, mget(campos_buenos)]
-
-library(dplyr)
-
-c1 <- c()
-c2 <- c()
-
-for( campo in  campos_buenos )
-{
-  #cat( campo, "  " )
-  qa = as.vector(t(dA[, mget(campo)]))
-  qb = as.vector(t(dB[, mget(campo)]))
-  t = ks.test(qa, qb)
-  c1 = append(c1,campo)
-  c2 = append(c2,t$p.value)
-  #sprintf(fmt = "Test: %f\n", t$p.value) %>% cat()
-}
-
-df_result = data.frame(c1, c2)
-df_result_order = df_result[order(df_result$c2, decreasing = TRUE),]
-df_result_order
 
 #-------------------------------------------------------------
 # Model Based
@@ -62,63 +64,71 @@ nrow(dAB)
 require("lightgbm")
 
 #https://www.rdocumentation.org/packages/ROCR/versions/1.0-11/topics/prediction
-#Since scoring classifiers give relative tendencies towards a negative (low scores) or positive (high scores) class, 
-#it has to be declared which class label denotes the negative, and which the positive class. Ideally, 
-#labels should be supplied as ordered factor(s), the lower level corresponding to the negative class, 
-#the upper level to the positive class. If the labels are factors (unordered), numeric, 
-#logical or characters, ordering of the labels is inferred 
-#from R's built-in < relation (e.g. 0 < 1, -1 < 1, 'a' < 'b', FALSE < TRUE). 
-#Use label.ordering to override this default ordering. Please note that the ordering can be locale-dependent e.g. for 
-#character labels '-1' and '1'.
 
-dt_lgb  <- lgb.Dataset( data=  data.matrix(dAB),
-                           label= dAB$real_clas)
+#Divido en train y test------------------:
+set.seed(621983)
+dt = sort(sample(nrow(dAB), nrow(dAB)*.7))
+df_train = dAB[dt,]
+df_test = dAB[-dt,]
+#----------------------------------------
+
+dt_lgb  <- lgb.Dataset( data=  data.matrix(df_train),
+                           label= df_train$real_clas)
 
 modelo  <- lightgbm( data= dt_lgb,
                      params= list( objective= "binary",
-                                   max_bin= 15, 
-                                   min_data_in_leaf= 4000,
-                                   learning_rate= 0.05,
+                              #     max_bin= 15, 
+                              #     min_data_in_leaf= 4000,
+                              #     learning_rate= 0.05,
                                    num_iterations = 100
                      )  )
 
-prediccion_training  <- predict( modelo,data.matrix(dAB))
+prediccion_training  <- predict( modelo,data.matrix(df_test))
 print(prediccion_training)
 
+df_test$predict = as.numeric(prediccion_training)
 
+print("-----------COMIENZO DE CORRIDA------------\n")
+nrow(df_test[df_test$real_clas == "A" & df_test$predict >= 0.9,]) #235354------------
+nrow(df_test[df_test$real_clas == "A"]) #235354       claseA (dA) = 235354
+print("--------\n")
 
-class(prediccion_training)
+nrow(df_test[df_test$real_clas == "B" & df_test$predict >= 0.9,]) #238986------------
+nrow(df_test[df_test$real_clas == "B",]) #238986   claseB (dB) = 238986
+print("-----------FIN DE CORRIDA------------\n")
 
-dAB$predict = prediccion_training
-dAB$predict = as.numeric(dAB$predict)
-head(dAB[, c("real_clas","predict")])
+df_test[,c("real_clas", "predict")]
 
-print("---------------------------------\n")
-nrow(dAB[dAB$real_clas == "A" & dAB$predict > 0.9,]) #235354------------
-dAB[dAB$real_clas == "A" & dAB$predict > 0.5,]
-nrow(dAB[dAB$real_clas == "A"]) #235354       claseA (dA) = 235354
-
-nrow(dAB[dAB$real_clas == "B" & dAB$predict < 0.5,]) #0
-
-nrow(dAB[dAB$real_clas == "B" & dAB$predict >= 0.9,]) #238986------------
-nrow(dAB[dAB$real_clas == "B",]) #238986   claseB (dB) = 238986
-
-nrow(dAB[dAB$real_clas == "B" & dAB$predict == 1,]) #0
-
-
-nrow(dAB[dAB$predict == 1]) #0
-nrow(dAB[dAB$predict > 0.99]) #474340 --->Predijo todo como "1"
-
-nrow(dAB[dAB$predict < 0.5]) #0
-
-#confusionMatrix(df_final_train$Category, pred_tr$lda$class)
-dAB[dAB$real_clas == 1]
-
-dAB[dAB$predict < 0.99]
-
+df_test
+#confusionMatrix(df_final_train$Category, pred_tr$lda$class
 #library(caret)
 #dAB[, "red_class"= ifelse(dAB$predict < 0.5, 0, 1)]
 #dAB[ , red_class  := as.numeric(prob_baja2 > 0.025) ]
+
+#-------------------------------------------------------------
+#--------Kolmogorov–Smirnov test (K–S test or KS test)--------
+# Do x and y come from the same distribution?
+#-------------------------------------------------------------
+
+library(dplyr)
+
+c1 <- c()
+c2 <- c()
+
+for( campo in  campos_buenos )
+{
+  #cat( campo, "  " )
+  qa = as.vector(t(dA[, mget(campo)]))
+  qb = as.vector(t(dB[, mget(campo)]))
+  t = ks.test(qa, qb)
+  c1 = append(c1,campo)
+  c2 = append(c2,t$p.value)
+  #sprintf(fmt = "Test: %f\n", t$p.value) %>% cat()
+}
+
+df_result = data.frame(c1, c2)
+df_result_order = df_result[order(df_result$c2, decreasing = TRUE),]
+df_result_order
 
 
 
