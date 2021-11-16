a39d9de4410886d0ef3da3e5c606cb4ced926c79
Claudia Aguirre <claudiaaguirre83@gmail.com>
2021-11-16 20:28:31 -0300

    se genera dataset reduccion 3 con lags y deltas del 1 al 4 y variables manuales del profesor y canaritos

--- src/bftf/952_dataset_epic.r
+++ src/bftf/952_dataset_epic.r
@@ -23,7 +23,7 @@ setwd( directory.root )
 palancas  <- list()  #variable con las palancas para activar/desactivar
 
 #palancas$version  <- "v952_exp11_8"   #Muy importante, ir cambiando la version
-palancas$version  <- "v952_exp13" 
+palancas$version  <- "v952_reduccion_3" 
 
 palancas$variablesdrift  <- c()
 #Se incluye columnas a sacar relacionadas con la  "importancia de variables".
@@ -40,20 +40,20 @@ palancas$variablesdrift  <- c()
 #                               "cinversion2", "cforex", "Master_mpagado", "ccheques_depositados", "tpaquete3", "Master_madelantopesos", "tpaquete4")   #aqui van las columnas que se quieren eliminar
 
 
-palancas$corregir <-  FALSE    # TRUE o FALSE
+palancas$corregir <-  TRUE    # TRUE o FALSE
 
 palancas$nuevasvars <-  TRUE  #si quiero hacer Feature Engineering manual
 
 palancas$dummiesNA  <-  FALSE #La idea de Santiago Dellachiesa
 
-palancas$lag1   <- FALSE    #lag de orden 1
-palancas$delta1 <- FALSE    # campo -  lag de orden 1 
-palancas$lag2   <- FALSE
-palancas$delta2 <- FALSE
-palancas$lag3   <- FALSE
-palancas$delta3 <- FALSE
-palancas$lag4   <- FALSE
-palancas$delta4 <- FALSE
+palancas$lag1   <- TRUE    #lag de orden 1
+palancas$delta1 <- TRUE    # campo -  lag de orden 1 
+palancas$lag2   <- TRUE
+palancas$delta2 <- TRUE
+palancas$lag3   <- TRUE
+palancas$delta3 <- TRUE
+palancas$lag4   <- TRUE
+palancas$delta4 <- TRUE
 palancas$lag5   <- FALSE
 palancas$delta5 <- FALSE
 palancas$lag6   <- FALSE
@@ -74,7 +74,7 @@ palancas$ratiomean6  <- FALSE   #Un derivado de la idea de Daiana Sparta
 palancas$tendencia6  <- FALSE    #Great power comes with great responsability
 
 
-palancas$canaritosimportancia  <- FALSE  #si me quedo solo con lo mas importante de canaritosimportancia
+palancas$canaritosimportancia  <- TRUE  #si me quedo solo con lo mas importante de canaritosimportancia
 
 
 #escribo para saber cuales fueron los parametros
@@ -615,9 +615,9 @@ correr_todo  <- function( palancas )
 {
   #cargo el dataset ORIGINAL
   #------------------------------------------------------------
-  #dataset  <- fread( "./datasetsOri/paquete_premium.csv.gz")
+  dataset  <- fread( "./datasetsOri/paquete_premium.csv.gz")
   #dataset  <- fread( "./datasets/dataset_epic_v952_exp9_6.csv.gz")
-  dataset  <- fread( "./datasets/dataset_epic_v952_reduccion_2.csv.gz")
+  #dataset  <- fread( "./datasets/dataset_epic_v952_reduccion_2.csv.gz")
   
   #------------------------------------------------------------
   
